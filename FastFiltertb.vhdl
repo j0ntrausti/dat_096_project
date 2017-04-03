ef9b743d8155392d3f83fa492268d53612aab176
Jón Trausti Kristmundsson <jontrausti22@gmail.com>
2017-04-03 16:48:49 +0200

    Add files via upload

--- FastFiltertb.vhdl
+++ FastFiltertb.vhdl
@@ -3,27 +3,27 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY FastFiltertb IS
-   GENERIC (WIDTH:INTEGER:=8);
-   PORT(y:OUT signed(2*WIDTH-1 DOWNTO 0));
+   GENERIC (WIDTH:INTEGER:=12);
+   PORT(y:OUT signed(WIDTH-1 DOWNTO 0));
 END FastFiltertb;
 
 ARCHITECTURE arch_FastFiltertb OF
                      FastFiltertb IS
 
    COMPONENT FastFilter IS
-      GENERIC(WIDTH:INTEGER:=8;
-              N:INTEGER:=4);
+      GENERIC(WIDTH:INTEGER:=12;
+              N:INTEGER:=188);
       PORT(reset:STD_LOGIC;
            clk:STD_LOGIC;
            clk250k:STD_LOGIC;
            clk6M:STD_LOGIC;
            x:IN signed(WIDTH-1 DOWNTO 0);
-           y:OUT signed(2*WIDTH-1 DOWNTO 0);
+           y:OUT signed(WIDTH-1 DOWNTO 0);
            finished:OUT STD_LOGIC);
    END COMPONENT FastFilter;
 
    SIGNAL x_tb_signal:signed(WIDTH-1 DOWNTO 0);
-   SIGNAL y_tb_signal:signed(2*WIDTH-1 DOWNTO 0);
+   SIGNAL y_tb_signal:signed(WIDTH-1 DOWNTO 0);
    SIGNAL clk:STD_LOGIC:='0';
    SIGNAL clk250k:STD_LOGIC:='0';
    SIGNAL clk6M:STD_LOGIC:='0';
@@ -48,35 +48,37 @@ BEGIN
 
 
 
-   x_tb_signal<=
-                "01000000" AFTER 725 ns, -- 0,5=64
-		"11100000" AFTER 1675 ns, -- -0,25=-32=224
-                "10100000" AFTER 2500 ns, -- -0,25=-32=224
-                "00110000" AFTER 3350 ns, -- 0,375=48
-		"01000000" AFTER 4200 ns, -- 0,5=64
-                "10100000" AFTER 5050 ns; -- -0,75=-96=160
+   x_tb_signal<="000000000000",
+                "010000000000" AFTER 4250 ns, -- 0,5=64
+		"111000000000" AFTER 4420 ns, -- -0,25=-32=224
+                "101000000000" AFTER 4590 ns, -- -0,25=-32=224
+                "001100000000" AFTER 4760 ns, -- 0,375=48
+		"010000000000" AFTER 4930 ns, -- 0,5=64
+                "101000000000" AFTER 5100 ns; -- -0,75=-96=160
 
 
    clk_proc:
    PROCESS
    BEGIN
-      WAIT FOR 50 ns;
+      WAIT FOR 10 ns;
       clk<=NOT(clk);
    END PROCESS clk_proc;
+
    clk_proc2:
    PROCESS
    BEGIN
-      WAIT FOR 850 ns;
+      WAIT FOR 170 ns;
       clk6M<=NOT(clk6M);
-	WAIT FOR 50 ns;
+	WAIT FOR 10 ns;
 	clk6M<=NOT(clk6M);
    END PROCESS clk_proc2;
+
    clk_proc22:
    PROCESS
    BEGIN
-      WAIT FOR 5450 ns;
+      WAIT FOR 4130 ns;
       clk250k<=NOT(clk250k);
-      wait for 50 ns;
+      wait for 10 ns;
 	clk250k<=NOT(clk250k);
    END PROCESS clk_proc22;
 
